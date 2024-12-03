6ee54136bbca33846a5839f62de80a33a71290e0
Nish <nish71427@gmail.com>
2024-12-03 00:53:04 -0600

    Improved code for Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,31 +3,22 @@ import java.io.File
 fun main() {
     val lines = File("./Day03.txt").readLines()
     val pattern = Regex("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\)")
-
-    val matches = pattern.findAll(lines.toString())
     var isEnabled = true
-
     var total = 0
-    for (m in matches) {
-        val value = m.value
 
-        when {
-            value == "do()" -> {
-                isEnabled = true
-            }
-            value == "don't()" -> {
-                isEnabled = false
-            }
-            value.startsWith("mul") -> {
-                val numbers = Regex("\\d+").findAll(value).map { it.value.toInt() }.toList()
-                if (numbers.size == 2 && isEnabled) {
-
-                    val (x, y) = numbers
-                    total += x * y
+    lines.forEach { line ->
+        pattern.findAll(line).forEach { matchResult ->
+            when {
+                matchResult.value == "do()" -> isEnabled = true
+                matchResult.value == "don't()" -> isEnabled = false
+                matchResult.value.startsWith("mul") -> {
+                    val (x, y) = matchResult.destructured.toList().map { it.toInt() }
+                    if (isEnabled) total += x * y
                 }
             }
         }
     }
+
 
     println(total)
 
